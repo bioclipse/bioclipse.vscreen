f961c1aedfc0418fd3e55bcaa405593748cb4ad7
Ola Spjuth <ola.spjuth@gmail.com>
2009-12-11 14:43:54 +0100

    Updated tests for vscreen (now using EP).

--- net.bioclipse.vscreen.tests/src/net/bioclipse/vscreen/tests/AbstractVScreenManagerPluginTest.java
+++ net.bioclipse.vscreen.tests/src/net/bioclipse/vscreen/tests/AbstractVScreenManagerPluginTest.java
@@ -14,12 +14,15 @@ import static org.junit.Assert.*;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.core.util.FileUtil;
 import net.bioclipse.structuredb.Activator;
 import net.bioclipse.structuredb.business.IJavaStructuredbManager;
 import net.bioclipse.vscreen.business.IVScreenManager;
+import net.bioclipse.vscreen.filters.IScreeningFilter;
 
 import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.CoreException;
@@ -36,7 +39,7 @@ import org.junit.Test;
 public abstract class AbstractVScreenManagerPluginTest {
 
 
-    protected static IVScreenManager managerNamespace;
+    protected static IVScreenManager vscreen;
     
     private final String TEST_DB_NAME="TestDB";
     private final String TEST_FILE = "/net/bioclipse/vscreen/tests/data/Fragments2.sdf";
@@ -54,8 +57,9 @@ public abstract class AbstractVScreenManagerPluginTest {
         sdb.createDatabase(TEST_DB_NAME);
         
         IFile file=null;
-        URL url = FileLocator.toFileURL(Platform.getBundle(net.bioclipse.vscreen.tests.Activator.PLUGIN_ID)
-                                        .getEntry(TEST_FILE));
+        URL url = FileLocator.toFileURL(Platform.getBundle(
+                                net.bioclipse.vscreen.tests.Activator.PLUGIN_ID)
+                                .getEntry(TEST_FILE));
         String path=url.getFile();
         file = FileUtil.createLinkedFile(path );
         
@@ -71,6 +75,34 @@ public abstract class AbstractVScreenManagerPluginTest {
         assertEquals("TEST DB does not contain 5 mols.", 5,
                      sdb.numberOfMoleculesInDatabaseInstance( TEST_DB_NAME ));
         
+    }
+
+    @Test
+    public void testFilterDB() throws BioclipseException {
+
+        sdb = Activator.getDefault().getStructuredbManager();
+
+        int nomols=sdb.numberOfMoleculesInDatabaseInstance( TEST_DB_NAME );
+        assertEquals("Incorrect number of mols in TEST DB.", 5, nomols );
+        
+        //Add some filters
+        List<IScreeningFilter>filters = new ArrayList<IScreeningFilter>();
+        filters.add(vscreen.createFilter("XlogP" , "<" , 2));
+
+        //Screen with filter and assert results
+        vscreen.filter(TEST_DB_NAME, filters, "filterDB1", "xlogpLT2" );
+        assertEquals("Incorrect number of mols in DB: filterDB1", 3, 
+                     sdb.numberOfMoleculesInDatabaseInstance( "filterDB1" ));
+
+        //Add more filters
+        filters.add(vscreen.createFilter("XlogP" , ">" , 1.5));
+        filters.add(vscreen.createFilter("Molweight" , "<" , 300));
+
+        //Screen with filter and assert results
+        vscreen.filter(TEST_DB_NAME, filters, "filterDB2", "xlogpLT2_GT1.5" );
+        assertEquals("Incorrect number of mols in DB: filterDB2", 4, 
+                     sdb.numberOfMoleculesInDatabaseInstance( "filterDB2" ));
+
     }
 
 }
