8a441432acc8721e936d12a741b9a4f6a4996d5e
Ola Spjuth <ola.spjuth@gmail.com>
2009-12-15 15:35:40 +0100

    Updated tests for new API (using EP). Does not work satisfactory for the moment.

--- net.bioclipse.vscreen.tests/src/net/bioclipse/vscreen/tests/AbstractVScreenManagerPluginTest.java
+++ net.bioclipse.vscreen.tests/src/net/bioclipse/vscreen/tests/AbstractVScreenManagerPluginTest.java
@@ -13,9 +13,13 @@ package net.bioclipse.vscreen.tests;
 import static org.junit.Assert.*;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+
+import junit.framework.TestFailure;
 
 import net.bioclipse.core.business.BioclipseException;
 import net.bioclipse.core.util.FileUtil;
@@ -46,21 +50,25 @@ public abstract class AbstractVScreenManagerPluginTest {
     IJavaStructuredbManager sdb;
     
     @Before
-    public void initialize() throws IOException, CoreException, BioclipseException{
+    public void initialize() throws IOException, CoreException, BioclipseException, URISyntaxException{
         
         sdb = Activator.getDefault().getStructuredbManager();
         
         //Set up and populate a StructureDB instance
         if ( sdb.allDatabaseNames().contains(TEST_DB_NAME) ) {
-        sdb.deleteDatabase(TEST_DB_NAME);
+            sdb.deleteDatabase(TEST_DB_NAME);
         }
         sdb.createDatabase(TEST_DB_NAME);
         
         IFile file=null;
-        URL url = FileLocator.toFileURL(Platform.getBundle(
-                                net.bioclipse.vscreen.tests.Activator.PLUGIN_ID)
-                                .getEntry(TEST_FILE));
-        String path=url.getFile();
+        URI uri = getClass().getResource(TEST_FILE).toURI();
+        URL url = FileLocator.toFileURL(uri.toURL());
+        String path = url.getFile();
+
+//        URL url = FileLocator.toFileURL(Platform.getBundle(
+//                                net.bioclipse.vscreen.tests.Activator.PLUGIN_ID)
+//                                .getEntry(TEST_FILE));
+//        String path=url.getFile();
         file = FileUtil.createLinkedFile(path );
         
         sdb.addMoleculesFromSDF(TEST_DB_NAME, file);
@@ -78,8 +86,8 @@ public abstract class AbstractVScreenManagerPluginTest {
     }
 
     @Test
-    public void testFilterDB() throws BioclipseException {
-
+    public void testXlogPFilter() throws BioclipseException {
+        
         sdb = Activator.getDefault().getStructuredbManager();
 
         int nomols=sdb.numberOfMoleculesInDatabaseInstance( TEST_DB_NAME );
@@ -90,19 +98,96 @@ public abstract class AbstractVScreenManagerPluginTest {
         filters.add(vscreen.createFilter("XlogP" , "<" , 2));
 
         //Screen with filter and assert results
-        vscreen.filter(TEST_DB_NAME, filters, "filterDB1", "xlogpLT2" );
-        assertEquals("Incorrect number of mols in DB: filterDB1", 3, 
-                     sdb.numberOfMoleculesInDatabaseInstance( "filterDB1" ));
+        vscreen.filter(TEST_DB_NAME, filters, "filterXLOGP", "xlogp" );
+        assertEquals(3, sdb.numberOfMoleculesInDatabaseInstance( "filterXLOGP" ));
 
-        //Add more filters
+        //Add another filters
         filters.add(vscreen.createFilter("XlogP" , ">" , 1.5));
-        filters.add(vscreen.createFilter("Molweight" , "<" , 300));
 
         //Screen with filter and assert results
-        vscreen.filter(TEST_DB_NAME, filters, "filterDB2", "xlogpLT2_GT1.5" );
-        assertEquals("Incorrect number of mols in DB: filterDB2", 4, 
-                     sdb.numberOfMoleculesInDatabaseInstance( "filterDB2" ));
+        vscreen.filter(TEST_DB_NAME, filters, "filterXLOGP2", "xlogp2" );
+        assertEquals(4, sdb.numberOfMoleculesInDatabaseInstance( "filterXLOGP2" ));
 
     }
+    
+    
 
+    @Test
+    public void testMWPFilter() throws BioclipseException {
+        
+        sdb = Activator.getDefault().getStructuredbManager();
+
+        int nomols=sdb.numberOfMoleculesInDatabaseInstance( TEST_DB_NAME );
+        assertEquals("Incorrect number of mols in TEST DB.", 5, nomols );
+        
+        //Add some filters
+        List<IScreeningFilter>filters = new ArrayList<IScreeningFilter>();
+        filters.add(vscreen.createFilter("MW" , "<" , 250));
+
+        //Screen with filter and assert results
+        vscreen.filter(TEST_DB_NAME, filters, "filterMW", "mw" );
+        assertEquals(3, sdb.numberOfMoleculesInDatabaseInstance( "filterMW" ));
+
+        //Add another filters
+        filters.add(vscreen.createFilter("MW" , ">" , 150));
+
+        //Screen with filter and assert results
+        vscreen.filter(TEST_DB_NAME, filters, "filterMW2", "xlogpMW2" );
+        assertEquals(4, sdb.numberOfMoleculesInDatabaseInstance( "filterMW2" ));
+
+    }
+    
+    @Test
+    public void testElementFilter() throws BioclipseException {
+        
+        sdb = Activator.getDefault().getStructuredbManager();
+
+        int nomols=sdb.numberOfMoleculesInDatabaseInstance( TEST_DB_NAME );
+        assertEquals("Incorrect number of mols in TEST DB.", 5, nomols );
+        
+        //Add some filters
+        List<IScreeningFilter>filters = new ArrayList<IScreeningFilter>();
+        filters.add(vscreen.createFilter("restrictElement", "C,N,O,Cl,S,F"));
+
+        //Screen with filter and assert results
+        vscreen.filter(TEST_DB_NAME, filters, "filterElement", "element" );
+        assertEquals(4, sdb.numberOfMoleculesInDatabaseInstance( "filterElement" ));
+
+        //Add another filters
+        filters.add(vscreen.createFilter("restrictElement", "C,N,O"));
+
+        //Screen with filter and assert results
+        vscreen.filter(TEST_DB_NAME, filters, "filterElement2", "element2" );
+        assertEquals(1, sdb.numberOfMoleculesInDatabaseInstance( "filterElement2" ));
+
+    }
+    
+    @Test
+    public void testRo5Filter() throws BioclipseException {
+        
+        sdb = Activator.getDefault().getStructuredbManager();
+
+        int nomols=sdb.numberOfMoleculesInDatabaseInstance( TEST_DB_NAME );
+        assertEquals("Incorrect number of mols in TEST DB.", 5, nomols );
+        
+        //Add some filters
+        List<IScreeningFilter>filters = new ArrayList<IScreeningFilter>();
+        
+        //Should have less than 2 failures
+        filters.add(vscreen.createFilter("RuleOfFive", "<=" , 2));
+
+        //Screen with filter and assert results
+        vscreen.filter(TEST_DB_NAME, filters, "filterRuleOfFive", "RuleOfFive" );
+        assertEquals(4, sdb.numberOfMoleculesInDatabaseInstance( "filterRuleOfFive" ));
+
+        //Add another filter, no failures now required
+        filters.add(vscreen.createFilter("RuleOfFive", "=" , 0));
+
+        //Screen with filter and assert results
+        vscreen.filter(TEST_DB_NAME, filters, "filterRuleOfFive2", "RuleOfFive2" );
+        assertEquals(1, sdb.numberOfMoleculesInDatabaseInstance( "filterRuleOfFive2" ));
+
+    }
+    
+    
 }
